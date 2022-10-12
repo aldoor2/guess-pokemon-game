9e54e2ab6699841009e36305ced2abf9cf650137
Alfonso Puerta <apuertaj2@gmail.com>
2022-10-11 22:42:46 -0500

    Added lazy loading

--- src/pages/Guess/Guess.tsx
+++ src/pages/Guess/Guess.tsx
@@ -1,7 +1,13 @@
-import * as React from 'react'
+import { useEffect, lazy, Suspense } from 'react'
 
 import { useGuess } from '@/context/GuessContext'
-import { DraggableElements, DroppableElements } from './components'
+
+const DraggableElementsLazy = lazy(
+  () => import('./components/DraggableElements')
+)
+const DroppableElementsLazy = lazy(
+  () => import('./components/DroppableElements')
+)
 
 function Guess() {
   const [
@@ -17,25 +23,29 @@ function Guess() {
     getAllPokemonFirstGeneration()
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     startGame()
   }, [])
 
   return (
     <>
       <h1 className='text-center text-2xl'>Adivina el pokemon!</h1>
-      <DraggableElements
-        pokemons={pokemons}
-        score={score}
-        maxDragggableElements={maxDragAndDropElements}
-        resetGame={startGame}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <DraggableElementsLazy
+          pokemons={pokemons}
+          score={score}
+          maxDragggableElements={maxDragAndDropElements}
+          resetGame={startGame}
+        />
+      </Suspense>
 
       <h2 className='text-center text-xl'>Arrastra y suelta</h2>
       {failedElementDropped && (
         <p className='text-white text-lg text-center'>Ups!</p>
       )}
-      <DroppableElements pokemons={pokemons} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <DroppableElementsLazy pokemons={pokemons} />
+      </Suspense>
     </>
   )
 }
